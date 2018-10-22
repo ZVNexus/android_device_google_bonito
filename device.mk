23ad201e8400bc59c5900db8a5481582894300e0
Christian Oder <myself5@carbonrom.org>
2018-10-22 13:09:56 -0400

    Don't enforce priv app whitelist
    Change-Id: Ib3cb798ace361a3565536148346533f314e373c3

--- device.mk
+++ device.mk
@@ -42,7 +42,8 @@ PRODUCT_COPY_FILES += \
 
 # Enforce privapp-permissions whitelist
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.control_privapp_permissions=enforce
+    ro.control_privapp_permissions=disable
+
 PRODUCT_COPY_FILES += \
     device/google/bonito/permissions/privapp-permissions-aosp.xml:system/etc/permissions/privapp-permissions-aosp.xml
 
