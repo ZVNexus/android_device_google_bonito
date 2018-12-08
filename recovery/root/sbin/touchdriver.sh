0605a004664e2024bdfcef2ff9e5c642f263ae54
bigbiff bigbiff <bigbiff@teamw.in>
2018-12-08 15:49:09 -0500

    b4s4: Grab touch modules from vendor.
    Saves space, which as we have come to know is practically a requirement.
    
    Change-Id: I1416394400941560838bb44310ed7b52eecbe1e0

--- recovery/root/sbin/touchdriver.sh
+++ recovery/root/sbin/touchdriver.sh
@@ -1,6 +1,9 @@
 #!/sbin/sh
 
-insmod /sbin/synaptics_dsx_core.ko
-insmod /sbin/synaptics_dsx_fw_update.ko
-insmod /sbin/synaptics_dsx_rmi_dev.ko
-insmod /sbin/synaptics_dsx_test_reporting.ko
+SLOT=`getprop ro.boot.slot_suffix`
+mount /dev/block/by-name/vendor$SLOT /vendor -o ro
+insmod /vendor/lib/modules/synaptics_dsx_core.ko
+insmod /vendor/lib/modules/synaptics_dsx_fw_update.ko
+insmod /vendor/lib/modules/synaptics_dsx_rmi_dev.ko
+insmod /vendor/lib/modules/synaptics_dsx_test_reporting.ko
+umount /vendor
