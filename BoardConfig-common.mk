b623f1ce0ced37f6902a141a130a09c108411382
ZVNexus <zvnexus@outlook.com>
2019-07-02 01:26:43 -0400

    Bump version to 3.3.1-2

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -240,6 +240,7 @@ BOARD_USES_SDM845_QCRIL := true
 -include vendor/google_devices/bonito/proprietary/BoardConfigVendor.mk
 
 # TWRP
+TW_DEVICE_VERSION := 2
 TW_THEME := portrait_hdpi
 BOARD_SUPPRESS_SECURE_ERASE := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
