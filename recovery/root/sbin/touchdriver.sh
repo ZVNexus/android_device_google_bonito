TeamWin/android_device_google_crosshatch

Insert touch modules at init to fix touch when booting to recovery.

recovery/root/sbin/touchdriver.sh
@@ -1,0 +1,4 @@
+#!/sbin/sh
+
+insmod /sbin/ftm5.ko
+insmod /sbin/sec_touch.ko

ZVNexus/android_device_google_bonito

recovery/root/sbin/touchdriver.sh
@@ -1,0 +1,6 @@
+#!/sbin/sh
+
+insmod /sbin/synaptics_dsx_core.ko
+insmod /sbin/synaptics_dsx_fw_update.ko
+insmod /sbin/synaptics_dsx_rmi_dev.ko
+insmod /sbin/synaptics_dsx_test_reporting.ko