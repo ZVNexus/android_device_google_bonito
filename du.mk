95600c0a285969010a413e90a99ecc7897588b72
J0SH1X <aljoshua.hell@gmail.com>
2019-09-14 15:18:04 -0400

    b4: Fix SafetyNet due to fingerprint mismatch.
    Change-Id: I7cd866a8fbf917bf3ca4d87f243ab7563b428748

--- du.mk
+++ du.mk
@@ -29,9 +29,14 @@ PRODUCT_MODEL := Pixel 3a XL
 PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=bonito \
-    BUILD_FINGERPRINT=google/bonito/bonito:10/QP1A.190711.020/5800535:user/release-keys \
+    PRODUCT_NAME="bonito" \
+    TARGET_DEVICE="bonito" \
     PRIVATE_BUILD_DESC="bonito-user 10 QP1A.190711.020 5800535 release-keys"
+
+BUILD_FINGERPRINT := "google/bonito/bonito:10/QP1A.190711.020/5800535:user/release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/bonito/bonito:10/QP1A.190711.020/5800535:user/release-keys
 
 BOARD_PREBUILT_VENDORIMAGE := vendor/images/bonito/vendor.img
 
