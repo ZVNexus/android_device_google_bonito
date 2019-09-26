1d552afccf036720b59e647230ce19c605a4cbcc
Alex Koskovich <zvnexus@outlook.com>
2019-09-26 01:14:08 -0400

    b4s4: Don't copy AOSP APNs
    Change-Id: Ia7a3cf7150e2ec757ee44904a510d792d9e8d7d6

--- aosp_sargo.mk
+++ aosp_sargo.mk
@@ -40,5 +40,4 @@ PRODUCT_DEVICE := sargo
 PRODUCT_MODEL := AOSP on sargo
 
 PRODUCT_COPY_FILES += \
-    device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml \
     $(LOCAL_PATH)/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
