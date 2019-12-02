52bb763f60811298a6cb7a889a353cf75b7221ce
ZVNexus <zvnexus@outlook.com>
2019-12-02 15:36:55 -0500

    Update fingerprint to QQ1A.191205.011.
    Change-Id: I7a1528702b9f79d402c4c211d41c982121aa1f55

--- du.mk
+++ du.mk
@@ -31,12 +31,12 @@ PRODUCT_MANUFACTURER := Google
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="bonito" \
     TARGET_DEVICE="bonito" \
-    PRIVATE_BUILD_DESC="bonito-user 10 QP1A.191105.003 5899767 release-keys"
+    PRIVATE_BUILD_DESC="bonito-user 10 QQ1A.191205.011 6009058 release-keys"
 
-BUILD_FINGERPRINT := "google/bonito/bonito:10/QP1A.191105.003/5899767:user/release-keys"
+BUILD_FINGERPRINT := "google/bonito/bonito:10/QQ1A.191205.011/6009058:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/bonito/bonito:10/QP1A.191105.003/5899767:user/release-keys
+    ro.build.fingerprint=google/bonito/bonito:10/QQ1A.191205.011/6009058:user/release-keys
 
 BOARD_PREBUILT_VENDORIMAGE := vendor/images/bonito/vendor.img
 
